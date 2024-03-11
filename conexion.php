14130477bd5de5c85455f7fb4f16d74562d5be63
Javiafb <jfernandezb02@gmail.com>
2024-03-11 12:58:59 -0500

    agregando algunos cambios

--- conexion.php
+++ conexion.php
@@ -1,4 +1,5 @@
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -9,3 +10,6 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
+
+mysqli_select_db($conn, $database) or die("no se encontro la base de datos");
+mysqli_set_charset($conn, "utf8");
